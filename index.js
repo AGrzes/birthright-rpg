b2f2c2265a30812eef58e4c588ee786e90f1d0a6
Grzegorz Abramczyk <abramczykg@gmail.com>
2016-09-23 22:50:35 +0200

    Initial application

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(process.env.OPENSHIFT_NODEJS_PORT || 8080, process.env.OPENSHIFT_NODEJS_IP || 'localhost', function () {
+    console.log('Example app listening on port 3000!');
+});
