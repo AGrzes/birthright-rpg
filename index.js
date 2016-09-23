d70a9c2d961da2a29f9e45ff0de9064b61651ba0
Grzegorz Abramczyk <abramczykg@gmail.com>
2016-09-23 22:59:22 +0200

    Health check

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
+app.get('/health', function (req, res) {
+    res.sendStatus(200);
+});
+
+
 app.listen(process.env.OPENSHIFT_NODEJS_PORT || 8080, process.env.OPENSHIFT_NODEJS_IP || 'localhost', function () {
     console.log('Example app listening on port 3000!');
 });
