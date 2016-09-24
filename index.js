39c37f250fe46f35d4e7970179400d7687963db8
Grzegorz Abramczyk <abramczykg@gmail.com>
2016-09-24 12:02:23 +0200

    Google ouath

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+var security = require('./security')(app, '655064932863-ipv5fbsc0hkua3sfqm22igimqhn5efj6.apps.googleusercontent.com', '2fH06-I9AKfSBg9ku3E78bvR', 'http://birthright.agrzes.pl/auth/google/callback', (profile) => {
+    return {id:profile};
+})
+
+app.get('/', security.secure, function (req, res) {
+    res.send('Hello World!' + JSON.stringify(req.user));
 });
 
 app.get('/health', function (req, res) {
---
+++ security.js
@@ -0,0 +1,63 @@
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+
+module.exports = function (app, clientID, clientSecret, callbackURL, lookup) {
+    passport.use(new GoogleStrategy({
+            clientID: clientID,
+            clientSecret: clientSecret,
+            callbackURL: callbackURL
+        },
+        function (accessToken, refreshToken, profile, cb) {
+            cb(null, lookup(profile.emails[0].value));
+        }
+    ));
+
+    passport.serializeUser(function (user, done) {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function (id, done) {
+        done(null, lookup(id));
+    });
+
+    app.use(require('cookie-parser')());
+    app.use(require('body-parser').urlencoded({
+        extended: true
+    }));
+    app.use(require('express-session')({
+        secret: 'keyboard cat',
+        resave: true,
+        saveUninitialized: true
+    }));
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    app.get('/auth/google',
+        passport.authenticate('google', {
+            scope: ['email']
+        }));
+
+    app.get('/auth/google/callback',
+        passport.authenticate('google', {
+            failureRedirect: '/login'
+        }),
+        function (req, res) {
+            // Successful authentication, redirect home.
+            res.redirect('/');
+        });
+
+    function ensureAuthenticated(req, res, next) {
+        if (req.isAuthenticated()) {
+            // req.user is available for use here
+            return next();
+        }
+
+        // denied. redirect to login
+        res.redirect('/auth/google')
+    }
+
+    return {
+        secure: ensureAuthenticated
+    }
+}
