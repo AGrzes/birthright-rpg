b2ef8b2f6bccba4c291a9cf7f0557ef5bc536849
Grzegorz Abramczyk <abramczykg@gmail.com>
2017-10-11 20:57:27 +0200

    Removed pouchdb router

--- pouchdb.js
+++ pouchdb.js
@@ -5,14 +5,10 @@ var fs = require('fs');
 PouchDB.plugin(require('pouchdb-adapter-memory'))
     .plugin(require('pouchdb-mapreduce'));
 
-var router = express.Router();
-router.pouch = PouchDB.defaults({
+var pouch = PouchDB.defaults({
     adapter: 'memory'
 })
 
-new router.pouch('birthright').bulkDocs(JSON.parse(fs.readFileSync('data.json'))).then(console.log).catch(console.error)
+new pouch('birthright').bulkDocs(JSON.parse(fs.readFileSync('data.json'))).then(console.log).catch(console.error)
 
-router.use(require('pouchdb-express-router')(router.pouch));
-
-
-module.exports = router;
+module.exports = pouch;
